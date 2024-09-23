eae2bbe6a5986f8f05da31ae921da0b0026d9679
SilvanoGPM <silvanosilvino@hotmail.com>
2024-09-23 01:51:58 -0300

    refactor: variáveis de ambiente funcionando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-'use client';
-
 import { Example } from '$components/example';
-import { env } from '$env';
 
 export default function Home() {
-  console.log('Aqui', env.NEXT_PUBLIC_APP_URL);
-
   return (
     <main className="flex flex-col items-center justify-center h-screen text-2xl font-bold">
       <Example />
