a67a4872d8b3b2b9f7038a8a01ff6da63aacc394
SilvanoGPM <silvanosilvino@hotmail.com>
2024-09-23 01:21:25 -0300

    refactor: million linter não utilizado por padrão

--- next.config.js
+++ next.config.js
@@ -1,19 +1,34 @@
-const MillionLint = require('@million/lint');
+// const MillionLint = require('@million/lint');
+
 const isProduction = process.env.NODE_ENV === 'production';
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
+  enabled: process.env.ANALYZE === 'true',
 });
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: !isProduction
+  disable: !isProduction,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
 };
-module.exports = MillionLint.next({
-  rsc: true
-})(withBundleAnalyzer(withPWA({
-  ...nextConfig
-})));
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    ...nextConfig,
+  }),
+);
+
+// Ativar caso deseje utilizar o linter million.
+
+// module.exports = MillionLint.next({
+//   rsc: true,
+// })(
+//   withBundleAnalyzer(
+//     withPWA({
+//       ...nextConfig,
+//     }),
+//   ),
+// );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import { Metadata } from 'next';
+'use client';
 
 import { Example } from '$components/example';
 import { env } from '$env';
 
-export const metadata: Metadata = {
-  title: 'Home',
-};
-
 export default function Home() {
-  console.log(env.NEXT_PUBLIC_APP_URL);
+  console.log('Aqui', env.NEXT_PUBLIC_APP_URL);
 
   return (
     <main className="flex flex-col items-center justify-center h-screen text-2xl font-bold">
