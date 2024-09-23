fd61666021b8af16ffa5d3ac75e912e6f1628a7f
SilvanoGPM <silvanosilvino@hotmail.com>
2024-09-23 01:04:05 -0300

    refactor: log da variável de ambiente para teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Metadata } from 'next';
 
 import { Example } from '$components/example';
+import { env } from '$env';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
 
 export default function Home() {
+  console.log(env.NEXT_PUBLIC_APP_URL);
+
   return (
     <main className="flex flex-col items-center justify-center h-screen text-2xl font-bold">
       <Example />
--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 export const env = createEnv({
   client: {
     NEXT_PUBLIC_MODE: z.literal('test').optional(),
+    NEXT_PUBLIC_APP_URL: z.string().optional(),
   },
 
   shared: {
@@ -13,5 +14,6 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_MODE: process.env.NEXT_PUBLIC_MODE,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 });
